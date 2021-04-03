2a89720f42ed539c446e98e8f477dbfacce1d768
matmoim <matmoiseenko@gmail.com>
2021-04-03 22:25:48 +0300

    commit

---
+++ src/kvadrat.js
@@ -0,0 +1,13 @@
+let user = 10;
+let acam = '';
+const ris = '*';
+
+for (let i = 0; i < user; i += 1) {
+    console.log(acam += ris );
+}
+
+
+
+for (let i = 0; i < user; i += 1) {
+    console.log(acam.slice(i));
+}
---
+++ src/shappppp.js
@@ -0,0 +1,6 @@
+const user = 6;
+let zvezda = '*';
+for (let i = 0; i < user; i += 1) {
+    '' += zvezda;
+}
+console.log(zvezda);
