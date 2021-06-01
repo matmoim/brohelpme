c085dbbfe5e0ab53612004c3c34c1107f6d490de
matmoim <matmoiseenko@gmail.com>
2021-06-01 09:42:16 +0300

    brovser

--- src/kvadrat.js
+++ src/kvadrat.js
@@ -1,11 +1,1 @@
-/**
-4 => ####
-3 => ###
-5 => #####
-*/
-let r = '';
-for(let i = 0; i < 4; i += 1 ){
-    r = r + '#';
-    
-}
-console.log(r);
+alert ('Hello , Brno');
--- src/shappppp.js
+++
@@ -1,6 +0,0 @@
-const user = 6;
-let zvezda = '*';
-for (let i = 0; i < user; i += 1) {
-    '' += zvezda;
-}
-console.log(zvezda);
